ce3e05182ddf2696db87cb0bd30d55e8b5c7c80f
MicFin <mrfinneran@gmail.com>
2017-02-23 22:11:10 -0500

    .age should be .handicap
    Close #34

--- js/event_handler_broken.js
+++ js/event_handler_broken.js
@@ -3,14 +3,14 @@
 
 const user = {
     data:[
-        {name:'T. Woods', age:37},
-        {name:'P. Mickelson', age:43}
+        {name:'T. Woods', handicap:37},
+        {name:'P. Mickelson', handicap:43}
     ],
     clickHandler:function () {
         // random number between 0 and 1
         let randomNum = ((Math.random () * 2 | 0) + 1) - 1,
         randomName,
-        randomAge;
+        randomHandicap;
 
         // Hey, what is the 'this' variable pointing to?
         debugger;
@@ -32,11 +32,11 @@ const user = {
         // what is property '1' of undefined?
         // data[1] is property '1' of undefined.
         randomName = this.data[randomNum].name;
-        randomAge = this.data[randomNum].age;
+        randomHandicap = this.data[randomNum].handicap;
 
-        // This line is printing a random person's name and age from
+        // This line is printing a random person's name and handicap from
         // the data array
-        console.log( randomName + ' ' + randomAge);
+        console.log( randomName + ' ' + randomHandicap);
     }
 };
 
